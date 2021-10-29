38f81c46ca4a5e721299ec70d871a10cdb01f79e
pierluigisupino <58226371+pierluigisupino@users.noreply.github.com>
2021-10-29 13:45:04 +0200

    Added SplashActivity

---
+++ app/src/main/java/com/ingsw2122_n_03/natour/presentation/SplashActivity.java
@@ -0,0 +1,32 @@
+package com.ingsw2122_n_03.natour.presentation;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import com.ingsw2122_n_03.natour.R;
+
+import java.util.Objects;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        Objects.requireNonNull(getSupportActionBar()).hide();
+
+        //SIMULATING SETUP
+        final Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashActivity.this, WelcomeActivity.class);
+                startActivity(intent);
+            }
+        }, 1000);
+    }
+}
