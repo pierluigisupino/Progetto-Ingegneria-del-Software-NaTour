ac85481cf48adb61f7b3513a7679ee5055cb298e
pierluigisupino <58226371+pierluigisupino@users.noreply.github.com>
2021-11-02 12:24:11 +0100

    Update

---
+++ app/src/main/java/com/ingsw2122_n_03/natour/presentation/LoginActivity.java
@@ -0,0 +1,33 @@
+package com.ingsw2122_n_03.natour.presentation;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
+
+import android.graphics.drawable.ColorDrawable;
+import android.os.Bundle;
+import android.text.SpannableString;
+import android.view.View;
+
+import com.google.android.material.appbar.MaterialToolbar;
+import com.ingsw2122_n_03.natour.R;
+
+import java.util.Objects;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        MaterialToolbar materialToolbar = (MaterialToolbar) findViewById(R.id.topAppBar);
+
+        materialToolbar.setNavigationOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
+
+    }
+}
--- app/src/main/java/com/ingsw2122_n_03/natour/presentation/WelcomeActivity.java
+++ app/src/main/java/com/ingsw2122_n_03/natour/presentation/WelcomeActivity.java
@@ -2,7 +2,10 @@ package com.ingsw2122_n_03.natour.presentation;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.ingsw2122_n_03.natour.R;
 
@@ -14,5 +17,15 @@ public class WelcomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
+
+        Button loginButton = (Button) findViewById(R.id.loginButton);
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(WelcomeActivity.this, LoginActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
