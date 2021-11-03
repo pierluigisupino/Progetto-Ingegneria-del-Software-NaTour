31ed9cd69aa5e20cbadfd194b75a7d08c6d76002
pierluigisupino <58226371+pierluigisupino@users.noreply.github.com>
2021-11-03 23:30:20 +0100

    Update Controller.java

--- app/src/main/java/com/ingsw2122_n_03/natour/application/Controller.java
+++ app/src/main/java/com/ingsw2122_n_03/natour/application/Controller.java
@@ -4,13 +4,10 @@ import android.app.Activity;
 import android.content.Intent;
 import android.util.Log;
 
-import com.amplifyframework.AmplifyException;
 import com.amplifyframework.auth.AuthProvider;
 import com.amplifyframework.core.Amplify;
 import com.ingsw2122_n_03.natour.domain.User;
 import com.ingsw2122_n_03.natour.presentation.ErrorActivity;
-import com.ingsw2122_n_03.natour.presentation.SplashActivity;
-import com.ingsw2122_n_03.natour.presentation.WelcomeActivity;
 
 public class Controller {
 
@@ -31,19 +28,6 @@ public class Controller {
 
     public User loginWithGoogle(Activity callingActivity){
         Amplify.Auth.signInWithSocialWebUI(AuthProvider.google(), callingActivity,
-                result -> Log.i("AuthQuickstart", result.toString()),
-                error -> {
-                    Intent intent = new Intent(callingActivity, ErrorActivity.class);
-                    callingActivity.startActivity(intent);
-                }
-        );
-
-        //DA COMPLETARE
-        return new User();
-    }
-
-    public User loginWithFacebook(Activity callingActivity){
-        Amplify.Auth.signInWithSocialWebUI(AuthProvider.facebook(), callingActivity,
                 result -> Log.i("AuthQuickstart", result.toString()),
                 error -> {
                     Intent intent = new Intent(callingActivity, ErrorActivity.class);
