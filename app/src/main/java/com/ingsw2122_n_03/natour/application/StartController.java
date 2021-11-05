fb88fa1a600dc4a7d0241105dc644a605f46cd8e
pierluigisupino <58226371+pierluigisupino@users.noreply.github.com>
2021-11-05 11:46:13 +0100

    Update Controller.java

--- app/src/main/java/com/ingsw2122_n_03/natour/application/StartController.java
+++ app/src/main/java/com/ingsw2122_n_03/natour/application/StartController.java
@@ -24,6 +24,7 @@ import com.ingsw2122_n_03.natour.presentation.VerifyAccount;
 import com.ingsw2122_n_03.natour.presentation.WelcomeActivity;
 import com.ingsw2122_n_03.natour.presentation.support.BaseActivity;
 
+import java.util.HashMap;
 import java.util.Objects;
 //gestisce routine di avvio dell'app, ma anche la connessione ad Amplify e le sue funzionalità (fare altra classe)
 public class StartController {
@@ -46,9 +47,29 @@ public class StartController {
         source.startActivity(intent);
     }
 
+    public void goToActivity(BaseActivity source, Class<?> destination, HashMap<String, String> extras){
+        Intent intent = new Intent(source, destination);
+
+        for(String key : extras.keySet()){
+            intent.putExtra(key, extras.get(key));
+        }
+
+        source.startActivity(intent);
+    }
 
     public void goToActivityAndFinish(BaseActivity source, Class<?> destination){
         Intent intent = new Intent(source, destination);
+        source.startActivity(intent);
+        source.finish();
+    }
+
+    public void goToActivityAndFinish(BaseActivity source, Class<?> destination, HashMap<String, String> extras){
+        Intent intent = new Intent(source, destination);
+
+        for(String key : extras.keySet()){
+            intent.putExtra(key, extras.get(key));
+        }
+
         source.startActivity(intent);
         source.finish();
     }
