7a321e939b5d3611fd535e3c2e48a140ebedc5c1
pierluigisupino <58226371+pierluigisupino@users.noreply.github.com>
2021-11-07 11:11:08 +0100

    Update AuthController.java

--- app/src/main/java/com/ingsw2122_n_03/natour/application/AuthController.java
+++ app/src/main/java/com/ingsw2122_n_03/natour/application/AuthController.java
@@ -168,7 +168,7 @@ public final class AuthController extends Controller {
     }
 
     public void onSignOutSuccess(){
-        mainActivity.onFail(welcomeActivity.getResources().getString(R.string.login_success));
+        mainActivity.onFail(welcomeActivity.getResources().getString(R.string.logout_success));
     }
 
     public void onSignOutFailure() {
