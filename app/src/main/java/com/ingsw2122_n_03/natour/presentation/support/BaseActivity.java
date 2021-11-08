17c118a497a76717ebde269b5f1a98ea20f4dfe2
pierluigisupino <58226371+pierluigisupino@users.noreply.github.com>
2021-11-08 13:32:05 +0100

    Update BaseActivity.java

--- app/src/main/java/com/ingsw2122_n_03/natour/presentation/support/BaseActivity.java
+++ app/src/main/java/com/ingsw2122_n_03/natour/presentation/support/BaseActivity.java
@@ -1,7 +1,5 @@
 package com.ingsw2122_n_03.natour.presentation.support;
 
-import android.widget.TextView;
-
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.chaos.view.PinView;
@@ -9,8 +7,8 @@ import com.google.android.material.textfield.TextInputEditText;
 
 public abstract class BaseActivity extends AppCompatActivity {
 
-    public abstract void onSuccess(String snackbarMessage);
-    public abstract void onFail(String snackbarMessage);
+    public abstract void onSuccess(String msg);
+    public abstract void onFail(String msg);
 
     public String getText(TextInputEditText textInputEditText) {
         return textInputEditText.getText().toString();
