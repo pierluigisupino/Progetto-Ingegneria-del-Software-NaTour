e23f8da9103a0ed4ed371bb5009484753e3582e6
pierluigisupino <58226371+pierluigisupino@users.noreply.github.com>
2021-12-12 19:43:52 +0100

    Update AddItineraryFragment2.java

--- app/src/main/java/com/ingsw2122_n_03/natour/presentation/AddItineraryFragment2.java
+++ app/src/main/java/com/ingsw2122_n_03/natour/presentation/AddItineraryFragment2.java
@@ -1,22 +1,23 @@
 package com.ingsw2122_n_03.natour.presentation;
 
 import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
-import android.widget.Spinner;
+import android.widget.AutoCompleteTextView;
 import android.widget.TimePicker;
-import android.widget.Toast;
-
-import androidx.annotation.Nullable;
-import androidx.fragment.app.Fragment;
 
 import com.ingsw2122_n_03.natour.R;
 
 public class AddItineraryFragment2 extends Fragment {
 
-    private Spinner difficultySpinner;
+    private View view;
+    private AutoCompleteTextView difficultyAutoCompleteTextView;
     private TimePicker timePicker;
     private int hour = 1;
     private int minutes = 0;
@@ -35,20 +36,31 @@ public class AddItineraryFragment2 extends Fragment {
     @Override
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
 
+        this.view = view;
         timePicker = view.findViewById(R.id.timePicker);
-        difficultySpinner = view.findViewById(R.id.difficultyMenu);
+        difficultyAutoCompleteTextView = view.findViewById(R.id.difficultyAutoComplete);
 
         timePicker.setIs24HourView(true);
         timePicker.setHour(hour);
         timePicker.setMinute(minutes);
+    }
 
-        ArrayAdapter<CharSequence> arrayAdapter = ArrayAdapter.createFromResource(view.getContext(), R.array.difficulties, R.layout.difficult_list_item);
-        difficultySpinner.setAdapter(arrayAdapter);
+    @Override
+    public void onResume() {
+        super.onResume();
 
+        String[] difficulties = new String[]{
+                "Turistico",
+                "Escursionistico",
+                "Esperto"
+        };
+
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(view.getContext(), R.layout.difficult_list_item, difficulties);
+        difficultyAutoCompleteTextView.setAdapter(arrayAdapter);
     }
 
     public String getDifficulty(){
-        return difficultySpinner.getSelectedItem().toString();
+        return difficultyAutoCompleteTextView.getText().toString();
     }
 
     public int getHours(){
@@ -59,14 +71,6 @@ public class AddItineraryFragment2 extends Fragment {
     public int getMinutes(){
         minutes = timePicker.getMinute();
         return minutes;
-    }
-
-    public boolean isDurationValid(){
-        if(getHours() != 0 || getMinutes() != 0){
-            return true;
-        }
-        Toast.makeText(getView().getContext(),"Insert a duration!!!", Toast.LENGTH_SHORT).show();
-        return false;
     }
 
 }
