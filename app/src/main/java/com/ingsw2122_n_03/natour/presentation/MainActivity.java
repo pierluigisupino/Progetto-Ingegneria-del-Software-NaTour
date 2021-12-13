4f21977b2e4a22f4357680134fae23888f47abdd
pierluigisupino <58226371+pierluigisupino@users.noreply.github.com>
2021-12-13 11:05:19 +0100

    Update MainActivity.java

--- app/src/main/java/com/ingsw2122_n_03/natour/presentation/MainActivity.java
+++ app/src/main/java/com/ingsw2122_n_03/natour/presentation/MainActivity.java
@@ -86,6 +86,7 @@ public class MainActivity extends BaseActivity implements NavigationView.OnNavig
         switch (item.getItemId()){
             case R.id.nav_logout:
                 authController.signOut(this);
+                break;
 
             //navigazione
         }
