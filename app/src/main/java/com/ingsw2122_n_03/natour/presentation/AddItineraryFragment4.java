8a24f6d8cfd45cac2cb57994bd630decc392c431
P0f509 <75103084+P0f509@users.noreply.github.com>
2021-12-22 23:04:43 +0100

    update map

--- app/src/main/java/com/ingsw2122_n_03/natour/presentation/AddItineraryFragment4.java
+++ app/src/main/java/com/ingsw2122_n_03/natour/presentation/AddItineraryFragment4.java
@@ -1,27 +1,46 @@
 package com.ingsw2122_n_03.natour.presentation;
 
+import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
-
-import androidx.activity.result.ActivityResultLauncher;
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.fragment.app.Fragment;
-
+import android.os.StrictMode;
 import android.preference.PreferenceManager;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.core.content.res.ResourcesCompat;
+import androidx.fragment.app.Fragment;
+
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.ingsw2122_n_03.natour.R;
 import com.ingsw2122_n_03.natour.databinding.Fragment4AddItineraryBinding;
 
 import org.osmdroid.api.IMapController;
+import org.osmdroid.bonuspack.routing.OSRMRoadManager;
+import org.osmdroid.bonuspack.routing.Road;
+import org.osmdroid.bonuspack.routing.RoadManager;
 import org.osmdroid.config.Configuration;
+import org.osmdroid.events.MapEventsReceiver;
 import org.osmdroid.tileprovider.tilesource.TileSourceFactory;
 import org.osmdroid.util.GeoPoint;
 import org.osmdroid.views.MapView;
+import org.osmdroid.views.overlay.MapEventsOverlay;
+import org.osmdroid.views.overlay.Marker;
+import org.osmdroid.views.overlay.Polyline;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
 
 public class AddItineraryFragment4 extends Fragment {
 
@@ -29,10 +48,14 @@ public class AddItineraryFragment4 extends Fragment {
     private FloatingActionButton searchButton;
     private FloatingActionButton addGPX;
 
-    private AddItineraryActivity addItineraryActivity;
+    private final AddItineraryActivity addItineraryActivity;
 
     private MapView map = null;
     private IMapController mapController;
+
+    private ArrayList<GeoPoint> waypoints = new ArrayList<>();
+
+    private ActivityResultLauncher<Intent> getGPXLauncher;
 
 
     public AddItineraryFragment4(AddItineraryActivity addItineraryActivity) {
@@ -42,6 +65,8 @@ public class AddItineraryFragment4 extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+        StrictMode.setThreadPolicy(policy);
     }
 
     @Override
@@ -55,6 +80,37 @@ public class AddItineraryFragment4 extends Fragment {
 
         searchButton = binding.searchButton;
         addGPX = binding.gpxButton;
+
+        getGPXLauncher = registerForActivityResult(new ActivityResultContracts.StartActivityForResult(),
+                result -> {
+                    if (result.getResultCode() == Activity.RESULT_OK) {
+                        Intent data = result.getData();
+                        assert data != null;
+                        Uri uri = data.getData();
+                        try {
+                            InputStream is = getView().getContext().getContentResolver().openInputStream(uri);
+                            BufferedReader bf = new BufferedReader(new InputStreamReader(is));
+                            StringBuilder sb = new StringBuilder();
+                            String readLines;
+                            while((readLines = bf.readLine()) != null) {
+                                sb.append(readLines+"\n");
+                            }
+                            is.close();
+                            Log.i("READ:", sb.toString());
+
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+
+                    }
+                });
+
+        addGPX.setOnClickListener(view -> {
+            Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+            intent.setType("*/*");
+            intent.addCategory(Intent.CATEGORY_OPENABLE);
+            getGPXLauncher.launch(intent);
+        });
 
         map = binding.map;
         map.setMultiTouchControls(true);
@@ -70,6 +126,31 @@ public class AddItineraryFragment4 extends Fragment {
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+
+        OSRMRoadManager roadManager = new OSRMRoadManager(view.getContext(), null);
+
+        MapEventsReceiver mReceiver = new MapEventsReceiver() {
+            @Override
+            public boolean singleTapConfirmedHelper(GeoPoint p) {
+                addWaypoint(p);
+                if(waypoints.size() > 1){
+                    Road road = roadManager.getRoad(waypoints);
+                    Polyline roadOverlay = RoadManager.buildRoadOverlay(road);
+                    map.getOverlays().add(roadOverlay);
+                    map.invalidate();
+                }
+                return false;
+            }
+
+            @Override
+            public boolean longPressHelper(GeoPoint p) {
+                return false;
+            }
+        };
+
+        MapEventsOverlay mEventOverlay = new MapEventsOverlay(mReceiver);
+        map.getOverlays().add(mEventOverlay);
+
     }
 
     @Override
@@ -82,6 +163,15 @@ public class AddItineraryFragment4 extends Fragment {
     public void onPause() {
         super.onPause();
         map.onPause();
+    }
+
+    private void addWaypoint(GeoPoint p) {
+        Marker marker = new Marker(map);
+        marker.setAnchor(Marker.ANCHOR_CENTER, Marker.ANCHOR_BOTTOM);
+        marker.setIcon(ResourcesCompat.getDrawable(getResources(), R.drawable.ic_circle, null));
+        map.getOverlays().add(marker);
+        marker.setPosition(p);
+        waypoints.add(p);
     }
 
 }
