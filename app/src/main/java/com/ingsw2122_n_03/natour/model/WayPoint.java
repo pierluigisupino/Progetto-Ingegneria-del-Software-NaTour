23569481b3b16fce846c5ca48da54953c97da104
pierluigisupino <58226371+pierluigisupino@users.noreply.github.com>
2021-12-28 12:45:51 +0100

    Update WayPoint.java

--- app/src/main/java/com/ingsw2122_n_03/natour/model/WayPoint.java
+++ app/src/main/java/com/ingsw2122_n_03/natour/model/WayPoint.java
@@ -20,7 +20,7 @@ public class WayPoint {
 
     @Override
     public String toString() {
-        return "{\"Latitude\": " + "\"" + this.getLatitude() + "\", \"Longitude\": "  + "\"" + this.getLongitude() + "\"}";
+        return "{\"Latitude\": " + "\"" + latitude + "\", \"Longitude\": "  + "\"" + longitude + "\"}";
     }
 
 
