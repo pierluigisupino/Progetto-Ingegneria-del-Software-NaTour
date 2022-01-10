6b91706a55a5a81cd6bb269c6bd76a6ee6ffbb15
P0f509 <75103084+P0f509@users.noreply.github.com>
2022-01-10 12:22:54 +0100

    update backend

--- backend/app.js
+++ backend/app.js
@@ -5,42 +5,36 @@ http://aws.amazon.com/apache2.0/
 or in the "license" file accompanying this file. This file is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and limitations under the License.
 */
+
 var express = require('express')
 var bodyParser = require('body-parser')
 var awsServerlessExpressMiddleware = require('aws-serverless-express/middleware')
 
-const { Client } = require('pg');
-const { decryptSecret } = require("utils");
-
-var host;
-var user;
-var port;
-var password;
-var database;
-var poolId;
-
-var client;
-
-const AWS = require('aws-sdk')
-const s3 = new AWS.S3()
-const cognito = new AWS.CognitoIdentityServiceProvider({
-    apiVersion: '2016-04-18'
+const {Client} = require('pg')
+const client = new Client({
+  host: process.env.DBHOST,
+  user: process.env.DBUSER,
+  port: process.env.DBPORT,
+  password: process.env.DBPASSWORD,
+  database: process.env.DBNAME
 })
 
+const AWS = require('aws-sdk')
+AWS.config.update({region: 'eu-west-3'})
+const s3 = new AWS.S3({apiVersion: '2006-03-01'})
+
+// declare a new express app
 var app = express()
-app.use(express.json({
-    limit: '50mb'
-}));
-app.use(express.urlencoded({
-    limit: '50mb'
-}));
+app.use(express.json({limit: '50mb'}));
+app.use(express.urlencoded({limit: '50mb'}));
 app.use(bodyParser.json())
 app.use(awsServerlessExpressMiddleware.eventContext())
 
+// Enable CORS for all methods
 app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*")
-    res.header("Access-Control-Allow-Headers", "*")
-    next()
+  res.header("Access-Control-Allow-Origin", "*")
+  res.header("Access-Control-Allow-Headers", "*")
+  next()
 });
 
 
@@ -60,33 +54,10 @@ app.get('/items/itineraries', function(req, res) {
       return res.json({Error : err.stack});
     }else {
       client.end();
-      return res.json(data.rows);
+      return res.json({Result: data.rows});
     }
   });
   
-});
-
-
-app.get('/items/user', async function(req, res) {
-  
-  await checkCredentials()
-  
-    var params = {
-        UserPoolId: poolId,
-        Username: req.query.id
-    };
-    cognito.adminGetUser(params, function(err, data) {
-        if (err)
-            return res.json({
-                Error: err
-            })
-        else
-         
-        return res.json(
-            (data.UserAttributes[2]).Value
-        );
-    });
-
 });
 
 
@@ -119,7 +90,7 @@ app.post('/items/itineraries', function(req, res) {
     }else {
       
       var iterID = suc.rows[0].iterid; 
-      var bucketName = 'natour-images-'+iterID;
+      var bucketName = 'natour-images-iter-'+iterID;
       
       var bucketParams = {
         Bucket : bucketName,
@@ -148,10 +119,9 @@ app.post('/items/itineraries', function(req, res) {
   
 });
 
-
 app.post('/items/photos', function(req, res) {
   
-  var bucketName = 'natour-images-'+req.body.iterID;
+  var bucketName = 'natour-images-iter-'+req.body.iterID;
   var count = req.body.photo_count;
   
   for(let i = 0; i < count; i++){
@@ -177,41 +147,45 @@ app.post('/items/photos', function(req, res) {
 
 
 /****************************
+* put methods *
 ****************************/
 
+
+app.put('/items', function(req, res) {
+  // Add your code here
+  res.json({success: 'put call succeed!', url: req.url, body: req.body})
+});
+
+app.put('/items/*', function(req, res) {
+  // Add your code here
+  res.json({success: 'put call succeed!', url: req.url, body: req.body})
+});
+
+
+/****************************
+* delete methods *
+****************************/
+
+
+app.delete('/items', function(req, res) {
+  // Add your code here
+  res.json({success: 'delete call succeed!', url: req.url});
+});
+
+app.delete('/items/*', function(req, res) {
+  // Add your code here
+  res.json({success: 'delete call succeed!', url: req.url});
+});
+
+
+/****************************
+****************************/
 
 app.listen(3000, function() {
     console.log("App started")
 });
 
-async function checkCredentials(){
-  if(typeof host == 'undefined' || typeof user == 'undefined' || typeof port == 'undefined' || typeof port == 'undefined' || typeof password == 'undefined' || typeof database == 'undefined' || typeof poolId == 'undefined'){
-    await getCredentials()
-  }
-}
-
-async function getCredentials(){
-  
-  host = await decryptSecret("host")
-  user = await decryptSecret("user")
-  port = await decryptSecret("port")
-  password = await decryptSecret("password")
-  database = await decryptSecret("database")
-  poolId = await decryptSecret("poolId")
-  
-  if(typeof client == 'undefined') await createClient()
-
-}
-
-async function createClient(){
-  
-  client = new Client({
-    host: host,
-    user: user,
-    port: port,
-    password: password,
-    database: database
-  })
-}
-
+// Export the app object. When executing the application local this does nothing. However,
+// to port it to AWS Lambda we will create a wrapper around that will load the app from
+// this file
 module.exports = app
