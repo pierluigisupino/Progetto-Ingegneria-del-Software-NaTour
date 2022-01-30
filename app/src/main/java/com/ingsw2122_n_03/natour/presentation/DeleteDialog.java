c5b05d2ddf0de734203567ac2d62dfef3ab41d79
pierluigisupino <58226371+pierluigisupino@users.noreply.github.com>
2022-01-30 12:22:49 +0100

    Update DeleteDialog.java

--- app/src/main/java/com/ingsw2122_n_03/natour/presentation/DeleteDialog.java
+++ app/src/main/java/com/ingsw2122_n_03/natour/presentation/DeleteDialog.java
@@ -2,13 +2,16 @@ package com.ingsw2122_n_03.natour.presentation;
 
 import android.app.AlertDialog;
 import android.app.Dialog;
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.Button;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatDialogFragment;
+import androidx.core.content.res.ResourcesCompat;
 
 import com.ingsw2122_n_03.natour.R;
 import com.ingsw2122_n_03.natour.application.IterController;
@@ -28,10 +31,26 @@ public class DeleteDialog extends AppCompatDialogFragment {
         builder.setView(view).setNegativeButton(R.string.no_text, null);
         builder.setView(view).setPositiveButton(R.string.yes_text, (dialog, which) -> IterController.getInstance().deleteItinerary());
 
-        AlertDialog dialog = builder.create();
+        final AlertDialog deleteDialog = builder.create();
 
-        dialog.getWindow().setBackgroundDrawableResource(R.drawable.rounded_dialog);
-        return dialog;
+        deleteDialog.setOnShowListener(dialog -> {
+
+            Button btnPositive = deleteDialog.getButton(AlertDialog.BUTTON_POSITIVE);
+            btnPositive.setTextSize(16);
+
+            Typeface typefacePositive = ResourcesCompat.getFont(requireContext(), R.font.euclid_circular_medium);
+            btnPositive.setTypeface(typefacePositive);
+
+            Button btnNegative = deleteDialog.getButton(AlertDialog.BUTTON_NEGATIVE);
+            btnNegative.setTextSize(16);
+
+            Typeface typefaceNegative = ResourcesCompat.getFont(requireContext(), R.font.euclid_circular_medium);
+            btnPositive.setTypeface(typefaceNegative);
+
+        });
+
+        deleteDialog.getWindow().setBackgroundDrawableResource(R.drawable.rounded_dialog);
+        return deleteDialog;
 
     }
 
