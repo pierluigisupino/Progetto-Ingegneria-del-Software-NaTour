e8fdb6ef44770cb78a35e46c8ab65fc9f8e6273d
pierluigisupino <58226371+pierluigisupino@users.noreply.github.com>
2022-02-05 17:07:17 +0100

    Update ChatFragment.java

--- app/src/main/java/com/ingsw2122_n_03/natour/presentation/chat/ChatFragment.java
+++ app/src/main/java/com/ingsw2122_n_03/natour/presentation/chat/ChatFragment.java
@@ -148,7 +148,6 @@ public class ChatFragment extends Fragment implements ChatAdapter.ItemClickListe
 
     @Override
     public void onItemClick(View view, int position) {
-        view.findViewById(R.id.animation_notify).setVisibility(View.GONE);
         messageController.onChatClick(chatAdapter.getItem(position));
     }
 
