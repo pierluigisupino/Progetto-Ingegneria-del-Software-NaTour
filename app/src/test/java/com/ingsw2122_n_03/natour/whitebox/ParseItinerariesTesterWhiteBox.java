4252c334ba5a2ba31869ef5a3c3d6b42c84dfcda
pierluigisupino <58226371+pierluigisupino@users.noreply.github.com>
2022-02-19 13:47:18 +0100

    Update ParseItinerariesTesterWhiteBox.java

--- app/src/test/java/com/ingsw2122_n_03/natour/whitebox/ParseItinerariesTesterWhiteBox.java
+++ app/src/test/java/com/ingsw2122_n_03/natour/whitebox/ParseItinerariesTesterWhiteBox.java
@@ -30,14 +30,31 @@ public class ParseItinerariesTesterWhiteBox {
         parseItinerariesMock = new ParseItinerariesMock();
     }
 
+    /*
+     *  Parsing of 2 itineraries with all fields set
+     *      - A json with 2 itineraries is created
+     *      - An arrayList with 2 itineraries equivalent to those of the json is created
+     *      - The correct result of parseItineraries will be an arrayList equivalent to the previous arrayList
+     */
     @Test
-    public void parseItineraryFullCoverage() throws JSONException {
+    public void parseItineraryFullFields() throws JSONException {
 
-        String itineraryJson = "[{\"iterid\":5,\"itername\":\"Roma\",\"description\":description,\"difficulty\":0,\"hours\":1,\"minutes\":0," +
-                "\"startpoint\":{\"x\":41.91215825489158,\"y\":12.492356197611912}," +
-                "\"waypoints\":[{\"Latitude\":\"41.90795968255628\",\"Longitude\":\"12.493655406267607\"}]," +
-                "\"creatorid\":\"7bba5c72-7fbe-45ad-996a-686c8685a9b8\",\"sharedate\":\"2022-02-15T00:00:00.000Z\"," +
-                "\"updatedate\":null,\"modifiedsince\":\"1644923584176\",\"creatorname\":\"User1\"}]";
+        String itineraryJson = "[" +
+                    "{" +
+                        "\"iterid\":5,\"itername\":\"Roma\",\"description\":description,\"difficulty\":0,\"hours\":1,\"minutes\":0," +
+                        "\"startpoint\":{\"x\":41.91215825489158,\"y\":12.492356197611912}," +
+                        "\"waypoints\":[{\"Latitude\":\"41.90795968255628\",\"Longitude\":\"12.493655406267607\"}]," +
+                        "\"creatorid\":\"7bba5c72-7fbe-45ad-996a-686c8685a9b8\",\"sharedate\":\"2022-02-15T00:00:00.000Z\"," +
+                        "\"updatedate\":\"2022-02-15T00:00:00.000Z\",\"modifiedsince\":\"1644923584176\",\"creatorname\":\"User1\"" +
+                    "}," +
+                    "{" +
+                        "\"iterid\":5,\"itername\":\"Roma\",\"description\":description,\"difficulty\":0,\"hours\":1,\"minutes\":0," +
+                        "\"startpoint\":{\"x\":41.91215825489158,\"y\":12.492356197611912}," +
+                        "\"waypoints\":[{\"Latitude\":\"41.90795968255628\",\"Longitude\":\"12.493655406267607\"}]," +
+                        "\"creatorid\":\"7bba5c72-7fbe-45ad-996a-686c8685a9b8\",\"sharedate\":\"2022-02-15T00:00:00.000Z\"," +
+                        "\"updatedate\":\"2022-02-15T00:00:00.000Z\",\"modifiedsince\":\"1644923584176\",\"creatorname\":\"User1\"" +
+                    "}," +
+                "]";
 
         Itinerary itinerary = new Itinerary(
                 "Roma",
@@ -50,6 +67,7 @@ public class ParseItinerariesTesterWhiteBox {
 
         itinerary.setIterId(5);
         itinerary.setDescription("description");
+        itinerary.setEditDate(Date.from(Instant.from(DateTimeFormatter.ISO_DATE_TIME.parse("2022-02-15T00:00:00.000Z"))));
 
         ArrayList<WayPoint> wayPoints = new ArrayList<>();
         wayPoints.add(new WayPoint(41.90795968255628, 12.493655406267607));
@@ -59,61 +77,56 @@ public class ParseItinerariesTesterWhiteBox {
 
         ArrayList<Itinerary> itineraries = new ArrayList<>();
         itineraries.add(itinerary);
+        itineraries.add(itinerary);
 
         ArrayList<Itinerary> result = parseItinerariesMock.parseItineraries(new JSONArray(itineraryJson));
         assertEquals(itineraries, result);
     }
 
+    /*
+     *  Parsing of 2 itineraries whit all the nullable fields set to null
+     *      - A json with 2 itineraries is created
+     *      - An arrayList with 2 itineraries equivalent to those of the json is created
+     *      - The correct result of parseItineraries will be an arrayList equivalent to the previous arrayList
+     */
     @Test
-    public void parseItineraryNoDescription() throws JSONException{
+    public void parseItineraryWithNullFields() throws JSONException {
 
-        String itineraryJson = "[{\"iterid\":5,\"itername\":\"Roma\",\"description\":null,\"difficulty\":0,\"hours\":1,\"minutes\":0," +
-                "\"startpoint\":{\"x\":41.91215825489158,\"y\":12.492356197611912}," +
-                "\"waypoints\":[{\"Latitude\":\"41.90795968255628\",\"Longitude\":\"12.493655406267607\"}]," +
-                "\"creatorid\":\"7bba5c72-7fbe-45ad-996a-686c8685a9b8\",\"sharedate\":\"2022-02-15T00:00:00.000Z\"," +
-                "\"updatedate\":null,\"modifiedsince\":\"1644923584176\",\"creatorname\":\"User1\"}]";
+        String itineraryJson = "[" +
+                    "{" +
+                        "\"iterid\":5,\"itername\":\"Roma\",\"description\":null,\"difficulty\":0,\"hours\":1,\"minutes\":0," +
+                        "\"startpoint\":{\"x\":41.91215825489158,\"y\":12.492356197611912}," +
+                        "\"waypoints\":null," +
+                        "\"creatorid\":\"7bba5c72-7fbe-45ad-996a-686c8685a9b8\",\"sharedate\":\"2022-02-15T00:00:00.000Z\"," +
+                        "\"updatedate\":null,\"modifiedsince\":\"1644923584176\",\"creatorname\":\"User1\"" +
+                    "}," +
+                    "{" +
+                        "\"iterid\":5,\"itername\":\"Roma\",\"description\":null,\"difficulty\":0,\"hours\":1,\"minutes\":0," +
+                        "\"startpoint\":{\"x\":41.91215825489158,\"y\":12.492356197611912}," +
+                        "\"waypoints\":null," +
+                        "\"creatorid\":\"7bba5c72-7fbe-45ad-996a-686c8685a9b8\",\"sharedate\":\"2022-02-15T00:00:00.000Z\"," +
+                        "\"updatedate\":null,\"modifiedsince\":\"1644923584176\",\"creatorname\":\"User1\"" +
+                    "}," +
+                "]";
+
+        Itinerary itinerary = new Itinerary(
+                "Roma",
+                0,
+                new LocalTime(1, 0),
+                new WayPoint(41.91215825489158, 12.492356197611912),
+                new User("7bba5c72-7fbe-45ad-996a-686c8685a9b8"),
+                Date.from(Instant.from(DateTimeFormatter.ISO_DATE_TIME.parse("2022-02-15T00:00:00.000Z")))
+        );
+
+        itinerary.setIterId(5);
+        itinerary.setModifiedSince(1644923584176L);
+        itinerary.getCreator().setName("User1");
+
+        ArrayList<Itinerary> itineraries = new ArrayList<>();
+        itineraries.add(itinerary);
+        itineraries.add(itinerary);
 
         ArrayList<Itinerary> result = parseItinerariesMock.parseItineraries(new JSONArray(itineraryJson));
-        assertFalse(result.isEmpty());
-        assertNull(result.get(0).getDescription());
+        assertEquals(itineraries, result);
     }
-
-    @Test
-    public void parseItineraryNoUpdateDate() throws JSONException{
-
-        String itineraryJson = "[{\"iterid\":5,\"itername\":\"Roma\",\"description\":description,\"difficulty\":0,\"hours\":1,\"minutes\":0," +
-                "\"startpoint\":{\"x\":41.91215825489158,\"y\":12.492356197611912}," +
-                "\"waypoints\":[{\"Latitude\":\"41.90795968255628\",\"Longitude\":\"12.493655406267607\"}]," +
-                "\"creatorid\":\"7bba5c72-7fbe-45ad-996a-686c8685a9b8\",\"sharedate\":\"2022-02-15T00:00:00.000Z\"," +
-                "\"updatedate\":null,\"modifiedsince\":\"1644923584176\",\"creatorname\":\"User1\"}]";
-
-        ArrayList<Itinerary> result = parseItinerariesMock.parseItineraries(new JSONArray(itineraryJson));
-        assertFalse(result.isEmpty());
-        assertNull(result.get(0).getEditDate());
-    }
-
-    @Test
-    public void parseItineraryNoWaypoints() throws JSONException{
-
-        String itineraryJson = "[{\"iterid\":5,\"itername\":\"Roma\",\"description\":description,\"difficulty\":0,\"hours\":1,\"minutes\":0," +
-                "\"startpoint\":{\"x\":41.91215825489158,\"y\":12.492356197611912}," +
-                "\"waypoints\":null," +
-                "\"creatorid\":\"7bba5c72-7fbe-45ad-996a-686c8685a9b8\",\"sharedate\":\"2022-02-15T00:00:00.000Z\"," +
-                "\"updatedate\":null,\"modifiedsince\":\"1644923584176\",\"creatorname\":\"User1\"}]";
-
-        ArrayList<Itinerary> result = parseItinerariesMock.parseItineraries(new JSONArray(itineraryJson));
-
-        assertFalse(result.isEmpty());
-        assertTrue(result.get(0).getWayPoints().isEmpty());
-    }
-
-    @Test
-    public void parseItineraryWithEmptyJson() throws JSONException{
-
-        String itineraryJson = "[]";
-
-        ArrayList<Itinerary> result =  parseItinerariesMock.parseItineraries(new JSONArray(itineraryJson));
-        assertTrue(result.isEmpty());
-    }
-
 }
